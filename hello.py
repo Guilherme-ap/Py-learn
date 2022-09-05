cb95948b579e5f9067a3f7d5e59ed84924dd2843
Guilherme Alves <guilherme.alvespp17@gmail.com>
2022-09-05 15:54:30 -0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,20 @@
+sum = 1 + 2
+print(sum)
+
+from datetime import date
+
+date.today()
+
+print ("Today's date is:" + str(date.today()))
+
+print("Welcome to the greeter program")
+name = input("Enter your name ")
+print("Greetings: " + name)
+
+
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(int(first_number) + int(second_number))
+
+#https://docs.microsoft.com/pt-br/learn/modules/python-create-manage-projects/2-set-up-project continuar daqui 
