4e5e5079b5308a4212827c6155da64b8d04760cb
Auxiliar_CPD <auxiliar_cpd@cipcia.com.br.local>
2023-06-05 10:41:33 -0300

    update archive hello - Py Harvad courses

---
+++ Documents/Guilherme/python/hello1.py
@@ -0,0 +1,14 @@
+#Ask user for their name
+name = input("what's your name? ").strip().title()
+
+#remove whitespace from str and capitalize user's name
+#name = name.strip().title()
+
+# capitalize user's name
+#name = name.capitalize()
+
+# title user's name
+#name = name.title()
+
+# Say hello to user
+print(f"hello, {name}")
